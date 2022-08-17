8bd4d97a1c0b013b48c0ea09379efe3375145182
Michael Marshall <michaelmarshall220@gmail.com>
2022-08-16 19:42:31 -0500

    added db version control functions from cord tables

--- src/main/kotlin/org/eten/databaseapi/core/AppConfig.kt
+++ src/main/kotlin/org/eten/databaseapi/core/AppConfig.kt
@@ -8,32 +8,18 @@ import java.net.InetAddress
 @Component
 @EnableConfigurationProperties
 class AppConfig(
-
     @Value("\${server.hostname}")
     val hostname: String,
-
-    @Value("\${server.jwtSecret}")
-    val jwt_secret: String,
-
-    @Value("\${awsAccessKeyId}")
-    val awsAccessKeyId: String,
-
-    @Value("\${awsSecretAccessKey}")
-    val awsSecretAccessKey: String,
-
-    @Value("\${email.server}")
-    val emailServer: String,
 
     @Value("\${env}")
     val env: ConfigEnv,
 
-    @Value("\${s3_url_prefix}")
-    val s3_url_prefix: String,
-
     @Value("\${spring.kafka.bootstrap-servers}")
     val kafka_bootstrap_address: String,
 
-    ) {
+    @Value("\${server.load-db-version}")
+    val load_db_version: Int = 1,
+) {
 
   val ip = InetAddress.getLocalHost().hostAddress
 
