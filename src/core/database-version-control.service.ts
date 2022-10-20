ffa700604022df456f3c8a147b264b27f90ae3e9
Daniel Leo <danielld707@gmail.com>
2022-10-20 19:45:36 -0400

    add script to load dummy data for showcase app

--- src/core/database-version-control.service.ts
+++ src/core/database-version-control.service.ts
@@ -1,75 +1,85 @@
-import { Injectable } from "@nestjs/common"
-import { readFileSync } from "fs"
-import { justBearerHeader } from "src/common/utility"
-import { PostgresService } from "./postgres.service"
+import { Injectable } from '@nestjs/common';
+import { readFileSync } from 'fs';
+import { justBearerHeader } from 'src/common/utility';
+import { PostgresService } from './postgres.service';
 
 @Injectable()
 export class DatabaseVersionControlService {
-  constructor(
-    private pg: PostgresService,
-  ) {
-    console.log("Database Version Control")
-    this.init()
+  constructor(private pg: PostgresService) {
+    console.log('Database Version Control');
+    this.init();
   }
 
   async init() {
-    const exists = await this.getIsDbInit()
+    const exists = await this.getIsDbInit();
 
     if (exists) {
-      const version = await this.getSchemaVersion()
-      console.log('Database schema version:', version)
+      const version = await this.getSchemaVersion();
+      console.log('Database schema version:', version);
     } else {
-      console.log("Creating database schema")
-      await this.toVersion1()
+      console.log('Creating database schema');
+      await this.toVersion1();
     }
 
-    console.log('Database version check complete')
+    console.log('Database version check complete');
   }
 
   async getIsDbInit(): Promise<boolean> {
-    const res = await this.pg.pool.query(`
+    const res = await this.pg.pool.query(
+      `
       SELECT EXISTS (
       SELECT FROM information_schema.tables 
       WHERE  table_name   = 'database_version_control');
-    `, [])
+    `,
+      [],
+    );
 
-    return res.rows[0].exists
+    return res.rows[0].exists;
   }
 
   async getSchemaVersion(): Promise<number> {
-    const res = await this.pg.pool.query(`
+    const res = await this.pg.pool.query(
+      `
       select version 
       from database_version_control 
       order by version 
       desc limit 1;
-    `, [])
+    `,
+      [],
+    );
 
-    const version = res.rows[0].version
+    const version = res.rows[0].version;
 
     if (version) {
-      return version
+      return version;
     }
 
-    return 0
+    return 0;
   }
 
   async toVersion1() {
     // schema
-    await this.runSqlFile('./src/core/sql/schema/v1.schema.sql')
+    await this.runSqlFile('./src/core/sql/schema/v1.schema.sql');
+
+    // load dummy data for showcase app
+    await this.runSqlFile('./src/core/sql/scripts/showcase.sql');
 
     // update db version
-    await this.setVersionNumber(1)
+    await this.setVersionNumber(1);
   }
 
   async setVersionNumber(version: number) {
-    const res = await this.pg.pool.query(`
+    const res = await this.pg.pool.query(
+      `
       insert into database_version_control(version) values($1);
-    `, [version])
+    `,
+      [version],
+    );
   }
 
   async runSqlFile(path: string) {
-    console.log('loading SQL:', path)
+    console.log('loading SQL:', path);
     const data = readFileSync(path, 'utf8');
-    const res = await this.pg.pool.query(data, [])
+    const res = await this.pg.pool.query(data, []);
   }
 }
---
+++ src/core/sql/scripts/showcase.sql
@@ -0,0 +1,33 @@
+-- Insert iso_639_3 for English language
+INSERT INTO iso_639_3(iso_639_3, part_2b, part_2t, part_1, scope, entry_type, ref_name)
+VALUES('eng', 'eng', 'eng', 'en', 'I', 'L', 'English')
+ON CONFLICT (id) DO NOTHING;
+
+-- Insert language skill
+INSERT INTO language_skills(user_id, language_table, language_id, skill_level)
+VALUES('user_id', 'iso_639_6', (SELECT id FROM iso_639_3 WHERE iso_639_3 = 'eng'), '5');
+
+-- Insert app
+INSERT INTO app_list(app_name)
+VALUES('app_item');
+
+-- Insert site text
+INSERT INTO site_text_keys(app, language_table, language_id, site_text_key, description)
+VALUES(
+  (SELECT id FROM app_list WHERE app_name = 'app_item'),
+  'iso_639_3',
+  (SELECT id FROM iso_639_3 WHERE iso_639_3 = 'eng'),
+  'site_text_key',
+  'description'
+);
+
+-- Insert site text translation
+INSERT INTO site_text_translations(
+  site_text, language_table, language_id, user_id, site_text_translation)
+VALUES(
+  (SELECT id FROM site_text_keys WHERE site_text_key = 'site_text_key'),
+  'iso_639_3',
+  (SELECT id FROM iso_639_3 WHERE iso_639_3 = 'eng'),
+  'user_id',
+  'site text translation'
+);
